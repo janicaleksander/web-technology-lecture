a3650eee4af841045993f5e5874248709a54686a
Aleksander <141730118+janicaleksander@users.noreply.github.com>
2025-11-27 21:53:02 +0100

    feat: Setting postgresql and .env file support

---
+++ Spring/Task02/src/main/resources/databaseStructure.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS kategoria(
+                                        ID INT,
+                                        nazwa VARCHAR(255) UNIQUE NOT NULL,
+                                        kod VARCHAR(255) UNIQUE NOT NULL,
+                                        CONSTRAINT kategoria_pk PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS produkt(
+                                      ID INT,
+                                      nazwa varchar(255) NOT NULL,
+                                      waga FLOAT NOT NULL CHECK ( waga > 0),
+                                      cena FLOAT NOT NULL CHECK ( cena > 0),
+                                      indeks_produktu  INT UNIQUE NOT NULL,
+                                      kategoria INT NOT NULL ,
+                                      CONSTRAINT produkt_pk PRIMARY KEY (ID),
+                                      CONSTRAINT kategoria_fk FOREIGN KEY (kategoria) REFERENCES kategoria(id) ON DELETE CASCADE
+);
